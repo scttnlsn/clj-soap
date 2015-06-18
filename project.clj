285a87ccc0ed5ace182c82103d7e2e09f1339cf7
Scott Nelson <scott@scttnlsn.com>
2015-06-18 13:07:38 -0400

    Update group for Clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.seancorfield/clj-soap "0.2.1-SNAPSHOT"
+(defproject org.clojars.scttnlsn/clj-soap "0.2.1-SNAPSHOT"
   :description "SOAP Client and Server using Apache Axis2."
   :url "https://github.com/seancorfield/clj-soap"
   :license {:name "Eclipse Public License"
@@ -10,4 +10,3 @@
                  [org.apache.axis2/axis2-transport-local "1.6.2"]]
   :source-paths ["src" "test"]
   :aot [clj-soap.test.core])
-
